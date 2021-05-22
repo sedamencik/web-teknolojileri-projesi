6b889e4a23ce71c75f7f8f21b68164048934a59e
seda mencik <sedamencikk@gmail.com>
2021-05-23 02:22:08 +0300

    .

---
+++ .vscode/js/app.js
@@ -0,0 +1,46 @@
+let key="1daf7993"
+
+const input=document.querySelector("#input");
+const button=document.querySelector("#button");
+const next=document.querySelector(".nextposter");
+const prev=document.querySelector(".nextposter");
+
+let value="";
+let pagenr=1;
+
+input.addEventListener("input",(e)=> {
+    e.preventDefault();
+    value=input.value;
+});
+
+button.addEventListener("click",()=>{
+    pagenr=1;
+    getmovie(value,pagenr);
+});
+
+async function getmovie(value,pagenr) {
+    if(value==="")return;
+    const data =await fetch(`http://www.omdbapi.com/?apikey=${key}&s=${value}&page=${pagenr}`);
+    document.querySelector(".display").innerHTML="";
+    const result=await data.json();
+    result.Search.forEach((item) => {
+        let moviediv=document.createElement("div");
+        moviediv.classList.add("movie");
+        let poster=document.createElement("div");
+        poster.classList.add("poster");
+        let img=document.createElement("img");
+        img.src=`${item.Poster}`==="N/A"?(img.src="../img/img.bulunamadi.jpg"):`${item.Poster}`;
+        poster.appendChild(img);
+        moviediv.appendChild(poster);
+
+        let description=document.createElement("div");
+        description.classList.add("description");
+        description.innerHTML=`Title : ${item.Title}<br><br>Yılı : ${item.Year}<br><br> <a href=https://www.imdb.com/title/${item.imdbID}> IMDB : https://www.imdb.com/title/${item.imdbID}</a>`;
+        
+        moviediv.appendChild(description);
+
+        document.querySelector(".display").appendChild(moviediv);
+
+    });
+    
+}
--- README.md
+++ README.md
@@ -8,23 +8,31 @@
 
 
     #### Ana sayfa
-    - Diğer sayfalara ulaşılabilecek menü
+    - Hakkımda bilgiler olan diğer sayfalara ulaşılabilecek menü
 
 
-    #### Hakkında sayfası
-    -Kişisel ilgi alanlarım, eğitim bilgilerim vb. bilgiler 
+    #### ÖzGeçmişim sayfası
+    - Okuduğum okulların tablosu
+
+    
+    #### İlgi Alanlarım sayfası
+    - Çizdiğim birkaç resim, favori filmlerim ve film arama motoru
 
 
     #### Şehrim sayfası
-    -Memleketim olan Kütahya'nın Simav ilçesinin kısaca tarihi geçmişinin, kültürel yapısının, doğal güzelliklerinin vb. görsellerle tanıtımı
+    - Memleketim olan Kütahya'nın Simav ilçesinin kısaca tarihi geçmişinin, kültürel yapısının, doğal güzelliklerinin vb. görsellerle tanıtımı
 
 
-    #### Görülmesi Gereken Doğal Güzellikler sayfası
-    -İlçeye gelen ama bilgisi olmayanlar için rehber niteliğinde tanıtım
+    #### Mirasımız sayfası
+    - Kütahya'nın ve Simav'ın mirasları
+
+
+    #### İletişim sayfası
+    - İsim ve E-posta kullanılarak mesaj gönderme sayfası
 
 
     #### Oturum Aç sayfası
-    -Kullanıcı girişinin istenmesi, onaylanmadığı zaman Oturum Aç sayfasına geri dönmesi
+    - Oturum açma işlemi
 
 ---
 
