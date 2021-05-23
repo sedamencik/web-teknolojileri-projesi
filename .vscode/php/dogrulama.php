17e13feeda1c3e832caedae63428e4527b98a1ba
seda mencik <sedamencikk@gmail.com>
2021-05-23 04:56:30 +0300

    .

---
+++ .vscode/php/dogrulama.php
@@ -0,0 +1,27 @@
+
+<?php
+
+error_reporting(0);
+
+if(isset($_POST['login'])){
+    $kulAd="g201210083";
+    $sifre="12345";
+    $kullaniciAdi=$_POST["kullaniciAdi"];
+    $parola=$_POST["parola"];
+    if($kullaniciAdi!="" && $parola!=""){
+        if($kullaniciAdi==$kulAd && $parola==$sifre){
+            echo "Hoşgeldiniz <br><hr>";
+            echo "$kullaniciAdi";
+        }
+        else{
+            header("<Location: ../oturumac.html");
+        }
+    }
+    else{
+        header("Location:../oturumac.html");
+    }
+}
+else {
+    header("Location:../oturumac.html");
+}
+?>
